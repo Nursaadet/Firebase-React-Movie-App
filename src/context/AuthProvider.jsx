69fce3e9ec42fe0d2aa666669f3c8efbb9650170
Nursaadet <nursaadetozdemir@gmail.com>
2025-09-15 17:09:18 +0300

    feat(auth): implement email/password sign-in with Firebase and error handling

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -26,7 +26,27 @@ const AuthProvider = ({ children }) => {
       console.log(error);
     }
   };
-  const values = { currentUse, createUser };
+
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  const signIn = async (email, password) => {
+    try {
+      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+
+      console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const values = { currentUse, createUser, signIn };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
