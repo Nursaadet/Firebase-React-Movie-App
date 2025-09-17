4dab74b615f3b87c73520ed3f81b75e25cc60eae
Nursaadet <nursaadetozdemir@gmail.com>
2025-09-18 00:56:26 +0300

    feat: add PrivateRouter component for protected routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
 import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
@@ -15,7 +16,7 @@ const AppRouter = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/details/:id" element={<MovieDetail />}>
+        <Route path="/details/:id" element={<PrivateRouter />}>
           <Route path="" element={<MovieDetail />} />
         </Route>
       </Routes>
